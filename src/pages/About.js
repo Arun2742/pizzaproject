9ea082f2a338888f4dfd5d5082d6d6535f3cd464
ARUN_J <itzmearun27@gmail.com>
2023-06-30 16:23:53 +0530

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,23 @@
+import React from "react";
+import MultiplePizzas from "../assets/multiplePizzas.jpeg";
+import "../styles/About.css";
+function About() {
+  return (
+    <div className="about">
+      <div
+        className="aboutTop"
+        style={{ backgroundImage: `url(${MultiplePizzas})` }}
+      ></div>
+      <div className="aboutBottom">
+        <h1> ABOUT US</h1>
+        <p>
+        There is  nothing cookie-cutter about Pedropizza. Not our pizzas. Not our people. And definitely not the way we live life. Around here, we don’t settle for anything less than food we’re proud to serve. And we don’t just clock in. Not when we can also become our best, make friends, and have fun while we’re at it. We’re the pizza company that lives life unboxed.
+        pizza, dish of Italian origin consisting of a flattened disk of bread dough topped with some combination of olive oil, oregano, tomato, olives, mozzarella or other cheese, and many other ingredients, baked quickly—usually, in a commercial setting, using a wood-fired oven heated to a very high temperature—and served hot.
+
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default About;
